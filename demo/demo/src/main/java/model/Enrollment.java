d44e5d1ea62b796f64486aafe685689357679644
Alexander <127439097+AlexanderMagichi@users.noreply.github.com>
2024-02-01 22:37:08 +0200

    Added class Lessons with columns

---
+++ demo/demo/src/main/java/model/Enrollment.java
@@ -0,0 +1,21 @@
+package model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Enrollment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long enrollmentID;
+    private Date enrollmentDate;
+
+    @ManyToOne
+    @JoinColumn(name = "userID")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "courseID")
+    private Course course;
+
+}
